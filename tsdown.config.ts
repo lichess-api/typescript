a3cfdecf288c4e03cf50d51f6a0dbef5f83ff676
Roman <dev@rman.dev>
2025-12-16 18:53:33 +0000

    Update tsdown.config.ts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,4 +4,5 @@ export default defineConfig({
   entry: { index: "src/index.ts", schemas: "src/schemas/index.ts" },
   exports: true,
   dts: { tsgo: true },
+  outputOptions: { minifyInternalExports: false },
 });
