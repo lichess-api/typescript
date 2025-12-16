d66dd0b0f0286c367a5e1cd99b45cd9fc4a9e041
Roman <dev@rman.dev>
2025-12-16 18:04:16 +0000

    update config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: { index: "src/index.ts", schemas: "src/schemas/index.ts" },
   exports: true,
-  unbundle: true,
-  dts: true,
+  dts: { tsgo: true },
 });
